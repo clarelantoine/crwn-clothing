df1397d5994848dc81a3617ebcda2e75d38bff22
Clarel Antoine <clarel.antoine93@gmail.com>
2023-07-04 02:17:32 +0400

    creating products context and product card

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,3 +1,19 @@
+import './shop.styles.scss';
+
+import { useContext } from 'react';
+import { ProductsContext } from '../../contexts/products.context';
+import ProductCard from '../../components/product-card/product-card.component';
+
 export default function Shop() {
-    return <h1>Shop here</h1>;
+    // get products context
+    const { products } = useContext(ProductsContext);
+
+    // create product cards based on the products context
+    return (
+        <div className="products-container">
+            {products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+            ))}
+        </div>
+    );
 }
